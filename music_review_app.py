4489c711879ea51ca0bdd7308634600965f637d7
Noah Williams <nw22122@my.westlake.school.nz>
2024-10-28 00:20:42 +1300

    song rating page works + designed, now need to do all songs page

--- music_review_app.py
+++ music_review_app.py
@@ -13,7 +13,12 @@ def get_db_connection():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM songs")
+    songs = cursor.fetchall()
+    conn.close()
+    return render_template('index.html', songs=songs)
 
 @app.route('/songs')
 def all_songs():
@@ -24,11 +29,28 @@ def all_songs():
     conn.close()
     return render_template('all_songs.html', songs=songs)
 
-@app.route('/rate')
-def song_rating():
+@app.route('/rate/<int:id>', methods=['GET', 'POST'])
+def song_rating(id):
     conn = get_db_connection()
-    cursor = conn.cursor()
-    song = cursor.execute('SELECT * FROM songs WHERE id = ?', (id,)).fetchone()
+    song = conn.execute('SELECT * FROM songs WHERE id = ?', (id,)).fetchone()
+
+    if request.method == 'POST':
+        song_rating = request.form.get('song_rating')
+        listen_again = request.form.get('listen_again')
+        recommend = request.form.get('recommend')
+        other_songs_by_artist = request.form.get('other_songs_by_artist')
+        lyrics_rating = request.form.get('lyrics_rating')
+        guessed_genre = request.form.get('guessed_genre')
+
+        if not song_rating or not listen_again or not recommend or not other_songs_by_artist or not lyrics_rating or not guessed_genre:
+            flash('All fields are required')
+        else:
+            conn.execute('INSERT INTO users-ratings (song_id, song_rating, listen_again, recommend, other_songs_by_artist, lyrics_rating, guessed_genre) VALUES (?, ?, ?, ?, ?, ?, ?)',
+                        (id, song_rating, listen_again, recommend, other_songs_by_artist, lyrics_rating, guessed_genre))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('all_songs'))
+
     conn.close()
     return render_template('song_rating.html', song=song)
 
