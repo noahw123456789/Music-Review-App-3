bf4855fdae444fc53f0e8d9c4f3ff27b59c0b618
noah <nw22122@my.westlake.school.nz>
2024-10-30 15:10:13 +1300

    Tables working, need to style

--- music_review_app.py
+++ music_review_app.py
@@ -48,16 +48,49 @@ def song_rating(id):
                         (rating, listen_again, mood, other_songs_by_artist, lyrics_rating))
             conn.commit()
             conn.close()
-            return redirect(url_for('all_songs'))
+            return redirect(url_for('view_ratings'))
 
     conn.close()
     return render_template('song_rating.html', song=song)
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit_rating(id):
+    conn = get_db_connection()
+    rating = conn.execute('SELECT * FROM ratings WHERE id = ?', (id)).fetchone()
+    
+    if request.method == 'POST':
+        rating = request.form.get('rating')
+        listen_again = request.form.get('listen_again')
+        mood = request.form.get('mood')
+        other_songs_by_artist = request.form.get('other_songs_by_artist')
+        lyrics_rating = request.form.get('lyrics_rating')
+
+        if not rating or not listen_again or not mood or not other_songs_by_artist or not lyrics_rating:
+            flash('Please fill out all fields!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE ratings set rating = ?, listen_again = ?, mood = ?, other_songs_by_artist = ?, lyrics_rating = ? WHERE id = ?,
+                        (rating, listen_again, mood, other_songs_by_artist, lyrics_rating))
+            conn.commit()
+            conn.close()
+
+            return redirect(url_for('view_ratings'))
+
+    conn.close()
+    return render_template('edit_rating.html', rating=rating )
+
+@app.route('/delete/<int:id>', methods=['GET', 'POST'])
+def delete_rating(id):
+    conn = get_db_connection()
+
+    conn.close()
+    return render_template('delete_rating.html')
 
 @app.route('/ratings')
 def view_ratings():
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM users_ratings")
+    cursor.execute("SELECT * FROM ratings")
     ratings = cursor.fetchall()
     conn.close()
     return render_template('view_ratings.html', ratings=ratings)
